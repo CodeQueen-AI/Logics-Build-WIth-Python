d91cf80ced606949f263d4b2756eda55af348daa
Sumbal Naz <Codeq209@gmail.com>
2025-07-04 16:12:43 +0500

    Add files via upload

---
+++ Q-29/index.py
@@ -0,0 +1,9 @@
+#Write a program that calculates the sum of digits of a given number
+num = int(input("Enter a number: "))
+
+sum_digits = 0
+while num > 0:
+    sum_digits += num % 10
+    num = num // 10 
+
+print(f"Sum of digits: {sum_digits}")
